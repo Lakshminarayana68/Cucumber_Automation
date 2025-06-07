9f1aeb9f2764e6d6c59d9b6ee3b0efe8a70d6f54
Lakshminarayana68 <lakshminarayanaindla464@gmail.com>
2025-06-07 20:20:35 +0530

    pom changes

---
+++ src/test/java/runners/ParallelRunner.java
@@ -0,0 +1,27 @@
+package runners;
+
+import org.testng.annotations.DataProvider;
+
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+
+@CucumberOptions(
+		
+		features= "src/test/resources/features",
+		glue = {"stepDefs", "hooks"},
+		plugin= {"pretty","listeners.CustomExtentReporter"},
+		monochrome=true
+		
+		
+		
+		)
+
+public class ParallelRunner extends AbstractTestNGCucumberTests {
+
+	
+	@Override
+	@DataProvider(parallel=true)
+	public Object[][] scenarios(){
+		return super.scenarios();
+	}
+}
